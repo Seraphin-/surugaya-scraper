d8adf10c3df92c5d020fd0acfef7b764d5b14c9e
unknown <ts.seraphin.osu@gmail.com>
2019-08-03 17:22:26 -0700

    use ThreadPool per process, and use a collection model - inserts are fast enough anyways, ok to be lazy with that

--- scrape.py
+++ scrape.py
@@ -1,5 +1,7 @@
 from multiprocessing import get_context
+from multiprocessing.pool import ThreadPool
 from requests_html import HTMLSession
+from requests.exceptions import ConnectionError
 import sqlite3
 from datetime import datetime
 from tqdm import tqdm, tqdm_gui
@@ -9,78 +11,85 @@ def log(base, name, itemid):
 	logfile.write(base + " " + name + " " + str(itemid) + "\n")
 	logfile.close()
 
+def get_retry(session, page):
+	active_page = False
+	attempt_count = 0
+	while not active_page:
+		try:
+			active_page = session.get(page)
+			attempt_count += 1
+		except ConnectionError:
+			if attempt_count > 10:
+				print("Gave up on page", page)
+				return False
+	return active_page
+
+def getPage(page):
+	global session
+	active_page = get_retry(session, page)
+	if active_page == False:
+		return
+	items = active_page.html.find('.item')
+	dirty = False
+	plist = []
+	for item in items:
+		itemid = int(item.find('.thum > a')[0].attrs['href'].split('/')[-1])
+		name = item.find('.title > a')[0].text
+		circle = item.find('.brand')[0].text
+		price = item.find('.item_price > .price_teika > span > strong')
+		condition = 0
+		if len(price) == 0:
+			price = -1
+		else:
+			price = int(price[0].text.replace(',', '').replace('￥', ''))
+			condition = item.find('.item_price > .price_teika')[0].text.split(':')[0]
+			if condition == "中古":
+				condition = 1 #used
+			else:
+				condition = 2 #new
+		image = item.find('.thum > a > img')[0].attrs['src']
+		release = item.find('.release_date')
+		if len(release) == 0:
+			release = "<Unknown>"
+		else:
+			release = release[0].text
+		if release == '発売日：/':
+			release = "<Unknown>" #another odd condition
+		status = item.find('.condition > span')
+		if len(status) == 0:
+			status = "none"
+		else:
+			status = status[0].attrs['class'][0] #hit, sale, new_arrival
+		if "price" in page and release != "<Unknown>" and circle != "GREen": #bugged with that one
+			dirty = True #no more no date releases to get for this price range
+		plist.append([itemid, name, circle, price, image, release, condition, status])
+	return (plist, dirty)
+
 def getCategory(category):
-	conn = sqlite3.connect('surugaya.db3')
-	conn.row_factory = sqlite3.Row
-	conn.execute('PRAGMA encoding="UTF-8";')
-	c = conn.cursor()
-	#Check if recently done (a hour)
-	# c.execute('SELECT * FROM log WHERE category = ? AND timestamp > ?', [category, datetime.now().timestamp() - 60 * 60 * 1])
-	# if c.fetchone() is not None:
-	# 	print('Skipping recently done', category)
-	# 	return
-	dirty = False #skip when true, used for avoiding extra duplication on price sort
+	global session
 	#Get first page manually and count total
 	session = HTMLSession()
 	BASE = "https://www.suruga-ya.jp"
-	first_page = session.get(BASE + category + '&inStock=On&adult_s=1')
+	first_page = get_retry(session, BASE + category + '&inStock=On&adult_s=1')
+	if first_page == False:
+		return
 	total, per = first_page.html.xpath('/html/body/div[1]/div[1]/div[2]/div[1]/div[1]')[0].search('該当件数:{}件中\xa01-{}件')
 	pages = -(-int(total.replace(',', '')) // int(per))
 	#print('Grabbing for', category, 'with total items', total)
-	for i in range(1, pages+1): #just reget the first one to make it easier
-		active_page = session.get(BASE + category + '&inStock=On&adult_s=1' + '&page=' + str(i))
-		items = active_page.html.find('.item')
-		for item in items:
-			itemid = int(item.find('.thum > a')[0].attrs['href'].split('/')[-1])
-			name = item.find('.title > a')[0].text
-			circle = item.find('.brand')[0].text
-			price = item.find('.item_price > .price_teika > span > strong')
-			condition = 0
-			if len(price) == 0:
-				price = -1
-			else:
-				price = int(price[0].text.replace(',', '').replace('￥', ''))
-				condition = item.find('.item_price > .price_teika')[0].text.split(':')[0]
-				if condition == "中古":
-					condition = 1 #used
-				else:
-					condition = 2 #new
-			image = item.find('.thum > a > img')[0].attrs['src']
-			release = item.find('.release_date')
-			if len(release) == 0:
-				release = "<Unknown>"
-			else:
-				release = release[0].text
-			if release == '発売日：/':
-				release = "<Unknown>" #another odd condition
-			status = item.find('.condition > span')
-			if len(status) == 0:
-				status = "none"
-			else:
-				status = status[0].attrs['class'][0] #hit, sale, new_arrival
+	pagelist = (BASE + category + '&inStock=On&adult_s=1' + '&page=' + str(i) for i in range(1, pages+1))
+	pool = ThreadPool(processes=8) #threads
+	r = pool.imap_unordered(getPage, pagelist)
+	pool.close()
+	dirty = False #skip when true, used for avoiding extra duplication on price sort
+	full = []
 
-			if "price" in category and release != "<Unknown>" and circle != "GREen": #bugged with that one
-				dirty = True #no more no date releases to get for this price range
-
-			c.execute('SELECT * FROM items WHERE productid = ?', [itemid])
-			row = c.fetchone()
-			props = [itemid, name, circle, price, image, release, condition, status]
-			if row is None:
-				c.execute('INSERT INTO items VALUES (?,?,?,?,?,?,?,?)', props)
-				log("New items:", name, itemid)
-			else:
-				update = False
-				for p in range(1, len(props)):
-					if props[p] != row[p] and row[0] != '186148292': #item with dupe search
-						log('Updated ' + row.keys()[p] + ':', props[1], props[0])
-						update = True
-				if update:
-					c.execute('UPDATE items SET name = ?, circle = ?, price = ?, image = ?, release = ?, condition = ?, status = ? WHERE productid = ?', props[1:] + [props[0]])
-		conn.commit()
+	for plist, dirty in r:
+		full += plist
 		if dirty:
-			#print("\nExited out of price set early, no more unknown date releases")
+			pool.terminate()
 			break
-	# c.execute('INSERT INTO log VALUES (?,?)', [category, datetime.now().timestamp()])
+	pool.join()
+	return full
 
 if __name__ == '__main__':
 	logfile = open(datetime.now().strftime("%Y-%m-%d-%H.log"), "a", encoding='utf-8')
@@ -92,9 +101,31 @@ if __name__ == '__main__':
 	categories = r.html.xpath('/html/body/div[1]/div[2]/div[2]/ul')[0].links #years
 	categories.update(r.html.xpath('/html/body/div[1]/div[2]/div[4]/ul')[0].links) #price
 	categories.add('/search?category=11010200&search_word=&rankBy=release_date%28int%29%3Aascending&restrict[]=price=[0,199]')
+	print("Getting all pages")
 	with get_context("spawn").Pool() as pool:
-		pool = Pool() #uses CPU count when processes=None
-		list(tqdm(enumerate(pool.imap_unordered(getCategory, categories)), desc='categories', total=len(categories)))
+		result = list(tqdm(enumerate(pool.imap_unordered(getCategory, categories)), desc='categories', total=len(categories)))
 		pool.close()
 		pool.join()
+	print("Updating database")
+	conn = sqlite3.connect('surugaya.db3', check_same_thread=True)
+	conn.row_factory = sqlite3.Row
+	conn.execute('PRAGMA encoding="UTF-8";')
+	c = conn.cursor()
+	for cat in result:
+		for props in cat[1]:
+			c.execute('SELECT * FROM items WHERE productid = ?', [props[0]])
+			row = c.fetchone()
+			if row is None:
+				c.execute('INSERT INTO items VALUES (?,?,?,?,?,?,?,?)', props)
+				log("New items:", props[1], props[0])
+			else:
+				update = False
+				for p in range(1, len(props)):
+					if props[p] != row[p] and row[0] != '186148292': #item with dupe search
+						log('Updated ' + row.keys()[p] + ':', props[1], props[0])
+						update = True
+				if update:
+					c.execute('UPDATE items SET name = ?, circle = ?, price = ?, image = ?, release = ?, condition = ?, status = ? WHERE productid = ?', props[1:] + [props[0]])
+			conn.commit()
+	conn.close()
 	print("Done")
